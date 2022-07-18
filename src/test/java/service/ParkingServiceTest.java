b90412a294367b49a77f29a9e49cd739cb4de75e
enekoe <enekose@gmail.com>
2022-07-18 19:02:31 +0300

    added closeParkingEvents test

--- src/test/java/service/ParkingServiceTest.java
+++ src/test/java/service/ParkingServiceTest.java
@@ -3,7 +3,6 @@ package service;
 import exception.CarAlreadyRegisteredException;
 import model.Car;
 import model.ParkingEvent;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
@@ -18,6 +17,8 @@ class ParkingServiceTest {
     private static final String NUMBER = "ABC123";
     private static final Integer PARKING_SLOTS = 5;
     private ParkingService parkingService;
+    private final LocalDateTime closeTime = LocalDateTime.of(
+            2000, 2,2,2,2,2,2);
     private Supplier<LocalDateTime> timeGenerator = () -> LocalDateTime.of(
             2000, 1, 1, 1, 1, 1, 1);
 
@@ -25,7 +26,7 @@ class ParkingServiceTest {
     @Test
     void registerCar() {
         var car = new Car(NUMBER);
-        var map = new HashMap<Car, LinkedList<ParkingEvent>>();
+        var map =new HashMap<Car, LinkedList<ParkingEvent>>();
         var expectedList = getExpectedEvents();
 
         parkingService = new ParkingServiceImpl(map, PARKING_SLOTS, timeGenerator);
@@ -61,12 +62,31 @@ class ParkingServiceTest {
     @Test
     void closeParkingEvent() {
         var car = new Car(NUMBER);
-        var map = new HashMap<Car, LinkedList<ParkingEvent>>();
-        var events = new LinkedList<ParkingEvent>();
-        events.add(new ParkingEvent(timeGenerator.get()));
-        map.put(car, events);
+        var map = GetSourceCarEvent(car);
+        var expectedEvents = expectedEventAfterClose();
+
         parkingService = new ParkingServiceImpl(map, PARKING_SLOTS, timeGenerator);
 
         parkingService.out(car);
+
+        assertThat(map).containsExactly(
+                entry(car, expectedEvents)
+        );
+    }
+
+    private LinkedList<ParkingEvent> expectedEventAfterClose() {
+        ParkingEvent expectedEvent = new ParkingEvent(timeGenerator.get());
+        expectedEvent.close(closeTime);
+        LinkedList<ParkingEvent> parkingEvents = new LinkedList<>();
+        parkingEvents.add(expectedEvent);
+        return parkingEvents;
+    }
+
+    private HashMap<Car, LinkedList<ParkingEvent>> GetSourceCarEvent(Car car) {
+        HashMap<Car, LinkedList<ParkingEvent>> map = new HashMap<>();
+        var events = new LinkedList<ParkingEvent>();
+        events.add(new ParkingEvent(timeGenerator.get()));
+        map.put(car, events);
+        return map;
     }
 }
